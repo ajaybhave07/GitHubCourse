5a9944ae8dacc06a64d639dd7cf63becb42414fe
ajay_bhave <bhaveajay007@gmail.com>
2022-03-09 17:46:03 +0530

    added LogOut page and modification

---
+++ src/Pages/LogOut.java
@@ -0,0 +1,11 @@
+package Pages;
+
+public class LogOut {
+
+	
+	
+	public void logOutFunctionality()
+	{
+		
+	}
+}
--- src/Pages/SearchPage.java
+++ src/Pages/SearchPage.java
@@ -2,10 +2,17 @@ package Pages;
 
 public class SearchPage {
 	
+	String driver;
 	public void search()
 	{
 		System.out.println("search method");
 		System.out.println("search box avaiable");
 	}
-
+	
+	SearchPage(String driver)
+	{
+		this.driver=driver;
+	}
+	
+	
 }
